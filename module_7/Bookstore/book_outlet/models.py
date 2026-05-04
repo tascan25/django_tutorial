9bf603301d310ba04b1a77a05712f7b71883c0c7
saksham.x.tyagi@goindigo.in <saksham.x.tyagi@goindigo.in>
2026-05-04 16:08:57 +0530

    completed the module_6 and module_7 which include the data, models and the admin panel information

--- module_7/Bookstore/book_outlet/models.py
+++ module_7/Bookstore/book_outlet/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=20)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="",null=False, db_index=True)
+    slug = models.SlugField(default="",null=False, db_index=True, blank=True)
 
     '''
     now django will create an table named, book in the database and it 
