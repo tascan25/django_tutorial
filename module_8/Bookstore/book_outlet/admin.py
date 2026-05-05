5b1d8b9c9229b9a0b878fa1f0b01522486a05332
Saksham Tyagi <sakshamtyagi@Sakshams-MacBook-Air.local>
2026-05-05 08:54:46 +0530

    latest code push

--- module_8/Bookstore/book_outlet/admin.py
+++ module_8/Bookstore/book_outlet/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 # Register your models here.
 
 
 
 class BookAdmin(admin.ModelAdmin):
     readonly_fields = ("slug",)
-    prepopulated_fields = {
-        "slug": ("title",)
-    }
     list_filter = ('author','rating')
+    list_display = ('title','author')
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
