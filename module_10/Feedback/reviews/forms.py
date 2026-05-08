12f947a83f60eb49db1ba9eeb8cff3089fc0b577
Saksham Tyagi <sakshamtyagi@Sakshams-MacBook-Air.local>
2026-05-08 15:29:52 +0530

    completed the module-10, of the curse and learned about the django forms and django model forms

--- module_10/Feedback/reviews/forms.py
+++ module_10/Feedback/reviews/forms.py
@@ -1,4 +1,49 @@
 from django import forms
+from .models import Reviews
 
-class ReviewForms(forms.Form):
-    user_name = forms.CharField()
+# class ReviewForms(forms.Form):
+#     user_name = forms.CharField(
+#         label="Your Name",
+#         required=True, 
+#         max_length=50,
+#         error_messages={
+#             "required":'Your name is required, you cannot leave it empty',
+#             "max_length":"please enter the shorter name"
+#         }
+#     )
+
+#     review_text = forms.CharField(
+#         label="Your Feedback", 
+#         required=True,
+#         max_length=200,
+#         error_messages={
+#             "required":"this field, is requried",
+#             "max_length":"please donot enter the more than 200 characters"
+#         },
+#         widget=forms.Textarea
+#     )
+
+#     rating = forms.IntegerField(min_value=1, max_value=5)
+
+class ReviewForms(forms.ModelForm):
+    class Meta:
+        model = Reviews
+        fields = "__all__"
+        labels = {
+            "user_name":"Your Name",
+            "review_text": "Your Feedback", 
+            "rating":"Your Rating",
+        }
+        error_messages = {
+            "user_name":{
+                "required":"this field is requried your feedback is valued here",
+                "max_length":"this field should not exceed the max length"
+            }, 
+            "review_text":{
+                "required":"This field is required, your feedback is valued here",
+                "max_length":"this field should not exceed the max length"
+            },
+            "rating":{
+                "required":"This field is required",
+            }
+        }
--- module_10/Feedback/reviews/models.py
+++ module_10/Feedback/reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Reviews(models.Model):
+    user_name = models.CharField(max_length=100)
+    review_text = models.TextField()
+    rating = models.IntegerField()
+    
+    #overriding the str dunder method the mdoels class
+    def __str__(self):
+        return f" username: {self.user_name}, review_text: {self.review_text}, rating: {self.rating}"
