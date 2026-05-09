e76ff57d320e5765b0c7a256408795de68665562
Saksham Tyagi <sakshamtyagi@Sakshams-MacBook-Air.local>
2026-05-09 23:19:05 +0530

    completed the module 11

--- module_11/Feedback/reviews/urls.py
+++ module_11/Feedback/reviews/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     # path('',views.index,name="starting_page"),
     path('',views.ReviewView.as_view(),name="starting_page"),
-    path('thankyou/<str:name>/',views.thank_you)
+    path('reviews/',views.ReviewsListView.as_view(),name="reviews_page"),
+    path('thankyou/<str:name>/',views.ThankYouReview.as_view(),name="thankyou_page"),
+    path('review/<int:pk>', views.SingleReviewView.as_view(),name="review_detail")
+   
 ]
--- module_11/Feedback/reviews/views.py
+++ module_11/Feedback/reviews/views.py
@@ -3,86 +3,41 @@ from django.http import HttpResponseRedirect
 from .forms import ReviewForms
 from .models import Reviews
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForms()
-        return render(request, 'reviews/review.html',{
-            "form":form
-        })
-    def post(self,request):
-        existing_form = Reviews.objects.get(pk=1)
-        entered_username = request.POST['user_name']
-        form = ReviewForms(request.POST, instance=existing_form) 
-        '''
-        now in the above initialization of the form class, we pass the already gathered form data 
-        to the form class, in order to let class validate the form. and if it does find something faulty
-        then it will generate the, corresponding errors for us
-        '''
-        
-        if form.is_valid():
-            '''
-            this is the method, which we are talking about in our above comment, 
-            '''
-            # review = Reviews(
-            #     user_name = form.cleaned_data['user_name'],
-            #     review_text = form.cleaned_data['review_text'],
-            #     rating = form.cleaned_data['rating']
-            # )
-            # review.save()
+class ReviewView(FormView):
+    form_class = ReviewForms
+    template_name = "reviews/review.html"
 
-            '''
-            now we don not need the above code if we are using the model form in order to create the model form, 
-            as while using the model form we can directly call the form.save() and it will automaticaly hanles all the things on 
-            it's own.
-            '''
-            form.save()
-            print(form.cleaned_data)
-            return HttpResponseRedirect(f"/thankyou/{entered_username}")
-        return render(request, 'reviews/review.html',{
-            "form":form
-        })
+    def form_valid(self, form):
+        form.save()
+        self.entered_username = form.cleaned_data['user_name']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return f"/thankyou/{self.entered_username}/"
+    
+   
+
+class ThankYouReview(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context["name"] = kwargs['name']
+        return context
 
 
-def index(request):
-    if request.method == 'POST':
-        existing_form = Reviews.objects.get(pk=1)
-        entered_username = request.POST['user_name']
-        form = ReviewForms(request.POST, instance=existing_form) 
-        '''
-        now in the above initialization of the form class, we pass the already gathered form data 
-        to the form class, in order to let class validate the form. and if it does find something faulty
-        then it will generate the, corresponding errors for us
-        '''
-        
-        if form.is_valid():
-            '''
-            this is the method, which we are talking about in our above comment, 
-            '''
-            # review = Reviews(
-            #     user_name = form.cleaned_data['user_name'],
-            #     review_text = form.cleaned_data['review_text'],
-            #     rating = form.cleaned_data['rating']
-            # )
-            # review.save()
+class ReviewsListView(ListView):
+    template_name = "reviews/reviews_list.html"
+    model = Reviews
 
-            '''
-            now we don not need the above code if we are using the model form in order to create the model form, 
-            as while using the model form we can directly call the form.save() and it will automaticaly hanles all the things on 
-            it's own.
-            '''
-            form.save()
-            print(form.cleaned_data)
-            return HttpResponseRedirect(f"/thankyou/{entered_username}")
-    else:
-        form = ReviewForms()
-        return render(request, 'reviews/review.html',{
-            "form":form
-        })
+    context_object_name = "reviewData"
 
-
-def thank_you(request, name):
-    return render(request, 'reviews/thank_you.html', {
-        "name": name
-    })
+    
+class SingleReviewView(DetailView):
+    template_name = "reviews/SingleReview.html"
+    model = Reviews
